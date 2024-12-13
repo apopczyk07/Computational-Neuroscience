94e6ef842d619f2e25d0202d696f66d1ec308b5d
apopczyk07 <anna.popczyk1@gmail.com>
2024-12-13 22:06:03 +0100

    kramers-kroning function added

--- kk_RID.py
+++ kk_RID.py
@@ -1,2 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def kramers_kronig_relation(frequencies, absorption_spectrum, ref_index_initial=1):
+    n_change = np.zeros_like(frequencies)
+    delta_omega = frequencies[1] - frequencies[0]  # Frequency step size
+
+    for i, omega in enumerate(frequencies):
+        integral = 0.0
+        for j, omega_prime in enumerate(frequencies):
+            if j != i:  # Skip the singularity
+                integral += (omega_prime * absorption_spectrum[j]) / (omega_prime**2 - omega**2) * delta_omega
+        n_change[i] = (2 / np.pi) * integral
+
+    return n_change + ref_index_initial
